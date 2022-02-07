ed84f8c9db32065b476778dc424561d6c208d49a
Taesoon Kim <lonelyhero77@gmail.com>
2022-02-07 15:03:09 +0900

    Optimize model

--- dlzerotoall/mnist.py
+++ dlzerotoall/mnist.py
@@ -18,11 +18,13 @@ Y = tf.placeholder(tf.float32, [None, nb_classes])
 
 W1 = tf.Variable(tf.random_normal([784, 1000]))
 b1 = tf.Variable(tf.random_normal([1000]))
+# layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
 layer1 = tf.nn.softmax(tf.matmul(X, W1) + b1)
 
 W2 = tf.Variable(tf.random_normal([1000, 1000]))
 b2 = tf.Variable(tf.random_normal([1000]))
-layer2 = tf.nn.softmax(tf.matmul(layer1, W2) + b2)
+layer2 = tf.sigmoid(tf.matmul(layer1, W2) + b2)
+# layer2 = tf.nn.softmax(tf.matmul(layer1, W2) + b2)
 
 W3 = tf.Variable(tf.random_normal([1000, nb_classes]))
 b3 = tf.Variable(tf.random_normal([nb_classes]))
@@ -36,7 +38,7 @@ is_correct = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 
 
-training_epochs = 100
+training_epochs = 300
 batch_size = 100
 total_batch = int(mnist.train.num_examples / batch_size)
 
